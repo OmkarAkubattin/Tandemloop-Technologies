be251f185c3a0cf537919b5c778176b0fdb911ab
Omkar Akubattin <57699070+OmkarAkubattin@users.noreply.github.com>
2025-05-21 18:06:47 +0530

    Create Problem-3.js

---
+++ Problem-3.js
@@ -0,0 +1,11 @@
+function generateOddSeriesConditional(a) {
+  const count = a % 2 === 0 ? a - 1 : a;
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    result.push(2 * i + 1);
+  }
+  return result;
+}
+
+// Example usage
+console.log("Output:", generateOddSeriesConditional(6).join(', ')); // 1, 3, 5, 7, 9
